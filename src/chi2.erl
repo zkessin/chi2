14a2845150f92cdd1c53105b9948a4bd767a79ee
Zachary Kessin <zkessin@gmail.com>
2013-10-04 15:07:26 +0300

    starting chi squared code

--- src/chi2.erl
+++ src/chi2.erl
@@ -1,6 +1,73 @@
 -module(chi2).
 
--export([my_func/0]).
+-include_lib("eunit/include/eunit.hrl").
+-export([get_total/1]).
 
-my_func() ->
-    ok.
+-compile(export_all).
+
+-type(chi2_row()    ::{integer(), integer()}).
+-type(chi2_vector() ::[chi2_row()]).
+
+-spec(get_total(chi2_vector()) -> number()).
+get_total(Vector) ->
+  lists:sum([F+S|| {F,S} <- Vector]).
+
+
+-spec(get_row_total(chi2_row()) -> integer()).
+get_row_total({F,S}) ->
+  F + S.
+
+-spec(get_degrees_of_freedom(chi2_vector()) -> integer()).
+get_degrees_of_freedom(V)->
+  length(V) -1.
+
+
+get_column_total(1,V) ->
+  lists:sum([F || {F,_S} <-V]);
+  
+get_column_total(2,V) ->
+  lists:sum([S || {_F,S} <-V]).
+
+get_ex_cell_values(V) ->
+  Total = get_total(V),
+  TF = get_column_total(1, V),
+  TS = get_column_total(2, V),
+  CellTotal = fun(RowTotal, ColTotal) ->
+    (RowTotal * ColTotal)/ Total
+  end,
+  lists:map(fun(Row) ->
+          RowTotal = get_row_total(Row),
+          {CellTotal(RowTotal, TF),
+           CellTotal(RowTotal, TS)}
+      end, V).
+
+
+-spec(square(number()) -> number()).
+square(E) -> E*E.
+
+
+compute_cell(O,E) ->
+  square(O - E) / E.
+
+compute_row({Fo, Fs},{Eo, Es}) ->
+  compute_cell(Fo, Eo) + 
+    compute_cell(Fs, Es).
+
+get_chi2_sum(V, E) ->
+  Combined = lists:zipwith(fun compute_row/2, E,V),
+    lists:sum(Combined).
+
+
+-spec(table(1|2, number) -> number() | corrilation_lt_threashhold).
+table(1, Sum) when Sum > 7.879  -> 0.995;
+table(1, Sum) when Sum > 6.635  -> 0.990;
+table(1, Sum) when Sum > 5.024  -> 0.990;
+table(1, Sum) when Sum > 3.841  -> 0.950;
+table(1, Sum) when Sum > 2.706  -> 0.900;
+table(2, Sum) when Sum > 10.597 -> 0.995;
+table(2, Sum) when Sum > 9.210  -> 0.990;
+table(2, Sum) when Sum > 7.378  -> 0.975;
+table(2, Sum) when Sum > 5.991  -> 0.950;
+table(2, Sum) when Sum > 4.605  -> 0.900;
+table(_,_) -> corrilation_lt_threashhold.
+
--- test/chi2_tests.erl
+++ test/chi2_tests.erl
@@ -1,3 +1,36 @@
 -module(chi2_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+get_sv(1)->
+  [{60, 300},
+   {10, 390}].
+
+get_total_test()->
+  ?assertEqual(chi2:get_total(get_sv(1)), 760).
+
+get_degrees_of_freedom_test() ->
+  ?assertEqual(chi2:get_degrees_of_freedom(get_sv(1)), 1).
+
+get_row_total_test() ->
+  ?assertEqual(chi2:get_row_total({70, 360}), 70+360).
+
+get_colum_total_test() ->
+  ?assertEqual(chi2:get_column_total(1, get_sv(1)), 70),
+  ?assertEqual(chi2:get_column_total(2, get_sv(1)), 690).
+
+get_ex_cell_value_test() ->
+  V  = get_sv(1),
+  E  = [{33.1578947368421,326.8421052631579},
+        {36.8421052631579,363.1578947368421}],
+  E2 = chi2:get_ex_cell_values(V),
+  ?assertEqual(E, E2).
+
+
+compute_cell_test() ->
+    ?assertEqual( 21.724535585042226 , chi2:compute_cell(60,33.16)).
+
+get_chi2_sum_test() ->
+  V    = get_sv(1),
+  E    = chi2:get_ex_cell_values(V),
+  Chi2 = chi2:get_chi2_sum(V, E),
+  ?assertEqual(88.30726614106119 , Chi2).
